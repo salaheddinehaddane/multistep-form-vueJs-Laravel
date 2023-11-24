378a2c540a21a9204091ef6ffc01d0c6e6f4ad8a
unknown <salahhhaddanepgmail.com>
2023-11-25 00:04:26 +0100

    update app.js + show form component content in browser

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,5 @@
 import './bootstrap';
 
 import { createApp } from 'vue';
-import App from './App.vue';
-createApp(App).mount("#app");
+import Form from './Components/Form.vue';
+createApp(Form).mount("#form");
